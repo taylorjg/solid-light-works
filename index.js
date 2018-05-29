abcb6df2f6d7b56612904c0b88c761a6ba619339
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-05-29 22:14:52 +0100

    Add second ellipse

--- index.js
+++ index.js
@@ -14,24 +14,42 @@ camera.position.set(0, 0, 5000);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 scene.add(camera);
 
-const ellipseCurve = new THREE.EllipseCurve(0, 0, 2.4, 2, 3.5 * Math.PI, 1.5 * Math.PI, true);
-const points = ellipseCurve.getPoints(500);
-const geometry = new THREE.Geometry().setFromPoints(points);
-const meshLine = new THREEMESHLINE.MeshLine();
-meshLine.setGeometry(geometry);
-const meshLineMaterial = new THREEMESHLINE.MeshLineMaterial({ lineWidth: 15 });
-const ellipse = new THREE.Mesh(meshLine.geometry, meshLineMaterial);
+const ellipseCurve1 = new THREE.EllipseCurve(-3, 0, 2.4, 2, 1.5 * Math.PI + (Math.PI / 900), 1.5 * Math.PI, true);
+const ellipseCurve2 = new THREE.EllipseCurve(3, 0, 2.4, 2, 1.5 * Math.PI, 1.5 * Math.PI, true);
+
+const points1 = ellipseCurve1.getPoints(500);
+const geometry1 = new THREE.Geometry().setFromPoints(points1);
+const meshLine1 = new THREEMESHLINE.MeshLine();
+meshLine1.setGeometry(geometry1);
+const meshLineMaterial1 = new THREEMESHLINE.MeshLineMaterial({ lineWidth: 15 });
+const ellipse1 = new THREE.Mesh(meshLine1.geometry, meshLineMaterial1);
+
+const points2 = ellipseCurve2.getPoints(500);
+const geometry2 = new THREE.Geometry().setFromPoints(points2);
+const meshLine2 = new THREEMESHLINE.MeshLine();
+meshLine2.setGeometry(geometry2);
+const meshLineMaterial2 = new THREEMESHLINE.MeshLineMaterial({ lineWidth: 15 });
+const ellipse2 = new THREE.Mesh(meshLine2.geometry, meshLineMaterial2);
 
 const leaving = new THREE.Group();
-leaving.add(ellipse);
+leaving.add(ellipse1);
+leaving.add(ellipse2);
 scene.add(leaving);
 
-const updateEllipse = () => {
-  ellipseCurve.aStartAngle -= (Math.PI / (180 * 60));
-  const points = ellipseCurve.getPoints(500);
-  geometry.setFromPoints(points);
-  geometry.verticesNeedUpdate = true;
-  meshLine.setGeometry(geometry);
+const updateEllipse1 = () => {
+  ellipseCurve1.aEndAngle -= (Math.PI / (180 * 60));
+  const points = ellipseCurve1.getPoints(500);
+  geometry1.setFromPoints(points);
+  geometry1.verticesNeedUpdate = true;
+  meshLine1.setGeometry(geometry1);
+};
+
+const updateEllipse2 = () => {
+  ellipseCurve2.aStartAngle -= (Math.PI / (180 * 60));
+  const points = ellipseCurve2.getPoints(500);
+  geometry2.setFromPoints(points);
+  geometry2.verticesNeedUpdate = true;
+  meshLine2.setGeometry(geometry2);
 };
 
 window.addEventListener("resize", () => {
@@ -47,25 +65,9 @@ const animate = () => {
   window.requestAnimationFrame(animate);
   const delta = clock.getDelta() * mixer.timeScale;
   mixer.update(delta);
-
-  updateEllipse();
-
+  updateEllipse1();
+  updateEllipse2();
   renderer.render(scene, camera);
 };
 
 animate();
-
-// const times = [0, 0.75 * DURATIONS[move] * speed];
-// const values = [];
-// const startQuaternion = new THREE.Quaternion();
-// const endQuaternion = END_QUATERNIONS[move];
-// startQuaternion.toArray(values, values.length);
-// endQuaternion.toArray(values, values.length);
-// const clip = new THREE.AnimationClip(
-//   move.name,
-//   -1,
-//   [new THREE.QuaternionKeyframeTrack(".quaternion", times, values)]);
-// const clipAction = mixer.clipAction(clip, sliceGroup);
-// clipAction.setLoop(THREE.LoopOnce);
-// clipAction.play();
-
