ab2b0d4d2f0c4a4b86dde63fbcfded9d41bebe5d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-06-13 19:58:52 +0100

    Show membrane normals

--- index.js
+++ index.js
@@ -98,8 +98,9 @@ const ellipseCurveLQ = new THREE.EllipseCurve(
   LEFT_END_ANGLE,
   ELLIPSE_CLOCKWISE);
 
-const ellipseGemoetryL = Line();
-const ellipseMeshL = new THREE.Mesh(ellipseGemoetryL, ellipseMaterial);
+const ellipseLineGeometryLP = Line();
+const ellipseLineGeometryLQ = Line();
+const ellipseMeshL = new THREE.Mesh(ellipseLineGeometryLQ, ellipseMaterial);
 
 // -------------
 // Right ellipse
@@ -123,8 +124,9 @@ const ellipseCurveRQ = new THREE.EllipseCurve(
   RIGHT_END_ANGLE,
   ELLIPSE_CLOCKWISE);
 
-const ellipseGemoetryR = Line();
-const ellipseMeshR = new THREE.Mesh(ellipseGemoetryR, ellipseMaterial);
+const ellipseLineGeometryRP = Line();
+const ellipseLineGeometryRQ = Line();
+const ellipseMeshR = new THREE.Mesh(ellipseLineGeometryRQ, ellipseMaterial);
 
 // --------------------------------
 // Membrane spotlights (projectors)
@@ -133,7 +135,7 @@ const ellipseMeshR = new THREE.Mesh(ellipseGemoetryR, ellipseMaterial);
 const spotLightTargetL = new THREE.Object3D();
 spotLightTargetL.position.set(LEFT_CENTRE_X, LEFT_CENTRE_Q_Y, 0);
 scene.add(spotLightTargetL);
-const spotLightL = new THREE.SpotLight(0xffffff, 200, MEMBRANE_LENGTH * 3, 20 * Math.PI / 180, 0);
+const spotLightL = new THREE.SpotLight(0xffffff, 200, MEMBRANE_LENGTH * 3, 22 * Math.PI / 180, 0);
 spotLightL.position.set(LEFT_CENTRE_X, LEFT_CENTRE_P_Y, MEMBRANE_LENGTH);
 spotLightL.target = spotLightTargetL;
 scene.add(spotLightL);
@@ -141,13 +143,23 @@ scene.add(spotLightL);
 const spotLightTargetR = new THREE.Object3D();
 spotLightTargetR.position.set(RIGHT_CENTRE_X, RIGHT_CENTRE_Q_Y, 0);
 scene.add(spotLightTargetR);
-const spotLightR = new THREE.SpotLight(0xffffff, 200, MEMBRANE_LENGTH * 3, 20 * Math.PI / 180, 0);
+const spotLightR = new THREE.SpotLight(0xffffff, 200, MEMBRANE_LENGTH * 3, 22 * Math.PI / 180, 0);
 spotLightR.position.set(RIGHT_CENTRE_X, RIGHT_CENTRE_P_Y, MEMBRANE_LENGTH);
 spotLightR.target = spotLightTargetR;
 scene.add(spotLightR);
 
+// const sideSpotTarget = new THREE.Object3D();
+// sideSpotTarget.position.set(0, 0, MEMBRANE_LENGTH / 2);
+// scene.add(sideSpotTarget);
+// const sideSpot = new THREE.SpotLight(0xffffff, 200);
+// sideSpot.position.set(0, 10, MEMBRANE_LENGTH / 2);
+// sideSpot.target = sideSpotTarget;
+// scene.add(sideSpot);
+
 let leftMembraneMesh;
 let rightMembraneMesh;
+let leftVertexNormalsHelper;
+let rightVertexNormalsHelper;
 
 const onTextureLoaded = hazeTexture => {
 
@@ -156,7 +168,7 @@ const onTextureLoaded = hazeTexture => {
 
   const hazeTextureMaterial = new THREE.MeshLambertMaterial({
     map: hazeTexture,
-    // side: THREE.FrontSide,
+    // side: THREE.DoubleSide,
     // color: 0x00dddd,
     color: 0xffffff,
     transparent: true,
@@ -179,12 +191,6 @@ const onTextureLoaded = hazeTexture => {
   rightMembraneMesh = new THREE.Mesh(rightMembraneGeometry, hazeTextureMaterial);
   scene.add(rightMembraneMesh);
 
-  const leftVertexNormalsHelper = new THREE.VertexNormalsHelper(leftMembraneMesh, 0.1, 0x00ff00);
-  scene.add(leftVertexNormalsHelper);
-
-  const rightVertexNormalsHelper = new THREE.VertexNormalsHelper(rightMembraneMesh, 0.1, 0x00ff00);
-  scene.add(rightVertexNormalsHelper);
-
   animate();
 };
 
@@ -195,6 +201,20 @@ textureLoader.load("haze.jpg", onTextureLoaded);
 // Animation
 // ---------
 
+let doOnceDone = false;
+
+const getPointsFromLineGeometry = (lineGeometry, z) => {
+  const positionAttribute = lineGeometry.getAttribute("position");
+  const array = positionAttribute.array;
+  const points = [];
+  for (let i = 0; i < array.length; i += positionAttribute.itemSize) {
+    const x = array[i];
+    const y = array[i + 1];
+    points.push(new THREE.Vector3(x, y, z));
+  }
+  return points;
+};
+
 const updateLeftForm = () => {
 
   ellipseCurveLP.aEndAngle -= ELLIPSE_ROTATION_DELTA;
@@ -203,12 +223,17 @@ const updateLeftForm = () => {
   const ellipsePointsLPVec2 = ellipseCurveLP.getPoints(ELLIPSE_POINT_COUNT);
   const ellipsePointsLQVec2 = ellipseCurveLQ.getPoints(ELLIPSE_POINT_COUNT);
 
+  const ellipsePointsLPArr = ellipsePointsLPVec2.map(vec2 => vec2.toArray());
   const ellipsePointsLQArr = ellipsePointsLQVec2.map(vec2 => vec2.toArray());
-  ellipseGemoetryL.update(ellipsePointsLQArr);
 
-  const lps = ellipsePointsLPVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, MEMBRANE_LENGTH));
-  const lqs = ellipsePointsLQVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, 0));
-  const tempGeometry = new MembraneBufferGeometry(lps, lqs, MEMBRANE_SEGMENT_COUNT);
+  ellipseLineGeometryLP.update(ellipsePointsLPArr);
+  ellipseLineGeometryLQ.update(ellipsePointsLQArr);
+
+  const ps = ellipsePointsLPVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, MEMBRANE_LENGTH));
+  const qs = ellipsePointsLQVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, 0));
+  // const ps = getPointsFromLineGeometry(ellipseLineGeometryLP, MEMBRANE_LENGTH);
+  // const qs = getPointsFromLineGeometry(ellipseLineGeometryLQ, 0);
+  const tempGeometry = new MembraneBufferGeometry(ps, qs, MEMBRANE_SEGMENT_COUNT);
   tempGeometry.computeVertexNormals();
   leftMembraneMesh.geometry.copy(tempGeometry);
   tempGeometry.dispose();
@@ -222,12 +247,17 @@ const updateRightForm = () => {
   const ellipsePointsRPVec2 = ellipseCurveRP.getPoints(ELLIPSE_POINT_COUNT);
   const ellipsePointsRQVec2 = ellipseCurveRQ.getPoints(ELLIPSE_POINT_COUNT);
 
+  const ellipsePointsRPArr = ellipsePointsRPVec2.map(vec2 => vec2.toArray());
   const ellipsePointsRQArr = ellipsePointsRQVec2.map(vec2 => vec2.toArray());
-  ellipseGemoetryR.update(ellipsePointsRQArr);
 
-  const rps = ellipsePointsRPVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, MEMBRANE_LENGTH));
-  const rqs = ellipsePointsRQVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, 0));
-  const tempGeometry = new MembraneBufferGeometry(rps, rqs, MEMBRANE_SEGMENT_COUNT);
+  ellipseLineGeometryRP.update(ellipsePointsRPArr);
+  ellipseLineGeometryRQ.update(ellipsePointsRQArr);
+
+  const ps = ellipsePointsRPVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, MEMBRANE_LENGTH));
+  const qs = ellipsePointsRQVec2.map(vec2 => new THREE.Vector3(vec2.x, vec2.y, 0));
+  // const ps = getPointsFromLineGeometry(ellipseLineGeometryRP, MEMBRANE_LENGTH);
+  // const qs = getPointsFromLineGeometry(ellipseLineGeometryRQ, 0);
+  const tempGeometry = new MembraneBufferGeometry(ps, qs, MEMBRANE_SEGMENT_COUNT);
   tempGeometry.computeVertexNormals();
   rightMembraneMesh.geometry.copy(tempGeometry);
   tempGeometry.dispose();
@@ -253,5 +283,18 @@ const animate = () => {
   updateLeftForm();
   updateRightForm();
   controls.update();
+  if (!doOnceDone) {
+    doOnceDone = true;
+    leftVertexNormalsHelper = new THREE.VertexNormalsHelper(leftMembraneMesh, 0.1, 0x00ff00);
+    rightVertexNormalsHelper = new THREE.VertexNormalsHelper(rightMembraneMesh, 0.1, 0x00ff00);
+    scene.add(leftVertexNormalsHelper);
+    scene.add(rightVertexNormalsHelper);
+  }
+  if (leftVertexNormalsHelper) {
+    leftVertexNormalsHelper.update();
+  }
+  if (rightVertexNormalsHelper) {
+    rightVertexNormalsHelper.update();
+  }
   renderer.render(scene, camera);
 };
