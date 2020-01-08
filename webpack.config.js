d66ebae167c6406dea82b2420dbd66912c02f87f
Administrator <jonathan.taylor.programmer@gmail.com>
2020-01-08 10:40:39 +0000

    Update build dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,34 @@
 /* eslint-env node */
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const packageJson = require('./package.json');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const { version } = require('./package.json')
 
-const dist = path.join(__dirname, 'dist');
+const dist = path.join(__dirname, 'dist')
 
 module.exports = {
-    entry: [
-        './src/index.js'
-    ],
-    output: {
-        path: dist,
-        filename: 'bundle.js',
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            { context: '.', from: '*.html' },
-            { context: '.', from: '*.css' },
-            { context: './assets', from: '*.png' },
-            { context: './assets', from: '*.jpg' },
-            { context: './assets', from: '*.jpeg' }
-        ]),
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            version: packageJson.version
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            }
-        ]
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: dist
-    }
-};
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: dist,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { context: '.', from: '*.html' },
+      { context: '.', from: '*.css' },
+      { context: './assets', from: '*.png' },
+      { context: './assets', from: '*.jpg' },
+      { context: './assets', from: '*.jpeg' }
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      version
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: dist
+  }
+}
