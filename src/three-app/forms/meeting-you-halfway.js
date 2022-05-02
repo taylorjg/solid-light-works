2e38813e6347701e307a4bf26955508d829a2c65
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-05-02 18:25:40 +0100

    Meeting You Halfway (7)

--- src/three-app/forms/meeting-you-halfway.js
+++ src/three-app/forms/meeting-you-halfway.js
@@ -11,30 +11,34 @@ const parametricEllipseY = ry =>
   t => ry * Math.sin(t)
 
 const ELLIPSE_POINT_COUNT = 100
-
 const CYCLE_TICKS = 10000
-
-const SWIPE_ROTATION_BLOCKS = [
-  { span: CYCLE_TICKS * 0.125, from: 0, to: -20 },
-  { span: CYCLE_TICKS * 0.125, from: -20, to: 0 },
-  { span: CYCLE_TICKS * 0.25, from: 0, to: 60 },
-  { span: CYCLE_TICKS * 0.25, from: 60, to: 0 },
-  { span: CYCLE_TICKS * 0.125, from: 0, to: -20 },
-  { span: CYCLE_TICKS * 0.125, from: -20, to: 0 }
-]
 
 export class MeetingYouHalfwayForm {
 
   constructor(width, height) {
     this.width = width
     this.height = height
-
-    this.swipeOffset = 0
-    this.swipeRotationDegrees = 0
     this.tick = 0
 
+    const swipeOffsetMax = width * 0.4
+    const swipeOffsetMin = -swipeOffsetMax
     const ryMax = height / 2
     const ryMin = height / 4
+
+    this.SWIPE_OFFSET_BLOCKS = [
+      { span: CYCLE_TICKS * 0.25, from: 0, to: swipeOffsetMax },
+      { span: CYCLE_TICKS * 0.5, from: swipeOffsetMax, to: swipeOffsetMin },
+      { span: CYCLE_TICKS * 0.25, from: swipeOffsetMin, to: 0 }
+    ]
+
+    this.SWIPE_ROTATION_BLOCKS = [
+      { span: CYCLE_TICKS * 0.125, from: 0, to: -20 },
+      { span: CYCLE_TICKS * 0.125, from: -20, to: 0 },
+      { span: CYCLE_TICKS * 0.25, from: 0, to: 60 },
+      { span: CYCLE_TICKS * 0.25, from: 60, to: 0 },
+      { span: CYCLE_TICKS * 0.125, from: 0, to: -20 },
+      { span: CYCLE_TICKS * 0.125, from: -20, to: 0 }
+    ]
 
     // B: slow expansion, fast contraction
     this.RY1_BLOCKS = [
@@ -58,56 +62,38 @@ export class MeetingYouHalfwayForm {
   }
 
   getLines() {
-    const modTick = this.tick % CYCLE_TICKS
-    const ratio = modTick / CYCLE_TICKS
+    const swipeOffset = linearRamps(this.SWIPE_OFFSET_BLOCKS, this.tick)
+    const swipeRotationDegrees = linearRamps(this.SWIPE_ROTATION_BLOCKS, this.tick)
+    const swipeRotationRadians = THREE.MathUtils.degToRad(swipeRotationDegrees)
 
     const rx = this.width / 2
-    const ry1 = linearRamps(this.RY1_BLOCKS, modTick)
-    const ry2 = linearRamps(this.RY2_BLOCKS, modTick)
-    this.swipeOffset = this.width / 4 * Math.sin(ratio * C.TWO_PI)
-    this.swipeRotationDegrees = linearRamps(SWIPE_ROTATION_BLOCKS, modTick)
+    const ry1 = linearRamps(this.RY1_BLOCKS, this.tick)
+    const ry2 = linearRamps(this.RY2_BLOCKS, this.tick)
 
-    const parametricEllipse1XFn = parametricEllipseX(rx)
-    const parametricEllipse1YFn = parametricEllipseY(ry1)
-    const parametricEllipse2XFn = parametricEllipseX(rx)
-    const parametricEllipse2YFn = parametricEllipseY(ry2)
-
-    const getEllipse1Points = (θ1, θ2) => {
-      const pointCount = ELLIPSE_POINT_COUNT
-      const Δθ = (θ2 - θ1) / pointCount
-      return U.range(pointCount + 1).map(n => {
-        const t = θ1 + n * Δθ
-        const x = parametricEllipse1XFn(t)
-        const y = parametricEllipse1YFn(t)
+    const getEllipsePoints = ry => {
+      const Δθ = C.TWO_PI / ELLIPSE_POINT_COUNT
+      return U.range(ELLIPSE_POINT_COUNT + 1).map(n => {
+        const t = n * Δθ
+        const x = parametricEllipseX(rx)(t)
+        const y = parametricEllipseY(ry)(t)
         return new THREE.Vector2(x, y)
       })
     }
 
-    const getEllipse2Points = (θ1, θ2) => {
-      const pointCount = ELLIPSE_POINT_COUNT
-      const Δθ = (θ2 - θ1) / pointCount
-      return U.range(pointCount + 1).map(n => {
-        const t = θ1 + n * Δθ
-        const x = parametricEllipse2XFn(t)
-        const y = parametricEllipse2YFn(t)
-        return new THREE.Vector2(x, y)
-      })
-    }
+    const ellipse1Points = getEllipsePoints(ry1)
+    const ellipse2Points = getEllipsePoints(ry2)
 
-    const ellipse1Points = getEllipse1Points(0, C.TWO_PI)
-    const ellipse2Points = getEllipse2Points(0, C.TWO_PI)
-
-    const normal = new THREE.Vector3(1, 0, 0)
-      .applyMatrix4(new THREE.Matrix4().makeRotationZ(THREE.MathUtils.degToRad(this.swipeRotationDegrees)))
-    const constant = this.swipeOffset
+    const rotationZ = new THREE.Matrix4().makeRotationZ(swipeRotationRadians)
+    const normal = new THREE.Vector3(1, 0, 0).applyMatrix4(rotationZ)
+    const constant = swipeOffset
     const wipeClippingPlane1 = new THREE.Plane(normal, constant)
     const wipeClippingPlane2 = wipeClippingPlane1.clone().negate()
     const line1 = new Line(ellipse1Points, { clippingPlanes: [wipeClippingPlane1] })
     const line2 = new Line(ellipse2Points, { clippingPlanes: [wipeClippingPlane2] })
+    const lines = [line1, line2]
 
     this.tick++
 
-    const lines = [line1, line2]
     return lines
   }
 }
