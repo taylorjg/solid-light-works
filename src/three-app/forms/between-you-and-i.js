5545e6441322921d0378066b6e50d74c8414e4b7
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-08-20 23:08:16 +0100

    Update Between You and I to use delta time

--- src/three-app/forms/between-you-and-i.js
+++ src/three-app/forms/between-you-and-i.js
@@ -5,9 +5,10 @@ import { parametricTravellingWaveX, parametricTravellingWaveY } from '../syntax/
 import * as C from '../constants'
 import * as U from '../utils'
 
+const MAX_TICKS = 20000
+const CYCLE_DURATION_MS = C.TICK_DURATION_MS * MAX_TICKS
 const ELLIPSE_POINT_COUNT = 200
 const TRAVELLING_WAVE_POINT_COUNT = 200
-const MAX_TICKS = 20000
 
 export class BetweenYouAndIForm {
 
@@ -16,12 +17,12 @@ export class BetweenYouAndIForm {
     this.height = height
     this.rx = width / 2 - C.LINE_THICKNESS / 2
     this.ry = height / 2 - C.LINE_THICKNESS / 2
-    this.tick = 0
+    this.accumulatedDurationMs = 0
     this.wipingInEllipse = initiallyWipingInEllipse
   }
 
-  getEllipsePoints(tickRatio) {
-    const ry = this.ry * 0.9 - this.ry * 0.7 * Math.sin(C.PI * tickRatio)
+  getEllipsePoints(cycleRatio) {
+    const ry = this.ry * 0.9 - this.ry * 0.7 * Math.sin(C.PI * cycleRatio)
     const Δθ = C.TWO_PI / ELLIPSE_POINT_COUNT
     return U.range(ELLIPSE_POINT_COUNT + 1).map(n => {
       const t = n * Δθ
@@ -31,12 +32,12 @@ export class BetweenYouAndIForm {
     })
   }
 
-  getTravellingWavePoints(tickRatio) {
+  getTravellingWavePoints(cycleRatio) {
     const λ = this.width
     const k = C.TWO_PI / λ
     const f = 1
     const ω = C.TWO_PI * f
-    const ωt = ω * tickRatio
+    const ωt = ω * cycleRatio
     const xoffset = -this.width / 2
     const Δx = this.width / TRAVELLING_WAVE_POINT_COUNT
     return U.range(TRAVELLING_WAVE_POINT_COUNT + 1).map(n => {
@@ -47,8 +48,8 @@ export class BetweenYouAndIForm {
     })
   }
 
-  getStraightLinePoints(tickRatio) {
-    const θ = THREE.MathUtils.degToRad(120) - C.PI * tickRatio
+  getStraightLinePoints(cycleRatio) {
+    const θ = THREE.MathUtils.degToRad(120) - C.PI * cycleRatio
     const px = this.width * Math.cos(θ)
     const py = this.height * Math.sin(θ)
     return [
@@ -57,17 +58,19 @@ export class BetweenYouAndIForm {
     ]
   }
 
-  getFootprintData() {
-    const tickRatio = this.tick / MAX_TICKS
+  getFootprintData(deltaMs) {
+    this.accumulatedDurationMs += deltaMs
+    const cycleRatio = this.accumulatedDurationMs / CYCLE_DURATION_MS
+
     const normal = new THREE.Vector3(1, 0, 0)
-    const constant = this.width / 2 - tickRatio * this.width
+    const constant = this.width / 2 - cycleRatio * this.width
     const wipingOutClippingPlane = new THREE.Plane(normal, constant)
     const wipingInClippingPlane = wipingOutClippingPlane.clone().negate()
     const scene1WipeClippingPlane = this.wipingInEllipse ? wipingInClippingPlane : wipingOutClippingPlane
     const scene2WipeClippingPlane = this.wipingInEllipse ? wipingOutClippingPlane : wipingInClippingPlane
-    const ellipsePoints = this.getEllipsePoints(tickRatio)
-    const travellingWavePoints = this.getTravellingWavePoints(tickRatio)
-    const straightLinePoints = this.getStraightLinePoints(tickRatio)
+    const ellipsePoints = this.getEllipsePoints(cycleRatio)
+    const travellingWavePoints = this.getTravellingWavePoints(cycleRatio)
+    const straightLinePoints = this.getStraightLinePoints(cycleRatio)
 
     const ellipseLine = new Line(ellipsePoints, {
       clippingPlanes: [scene1WipeClippingPlane]
@@ -81,18 +84,19 @@ export class BetweenYouAndIForm {
       clipToFormBoundary: true
     })
 
-    this.tick++
-    if (this.tick > MAX_TICKS) {
-      this.toggleWipeMode()
-    }
     const lines = [ellipseLine, travellingWaveLine, straightLine]
 
     const footprintData = { lines }
+
+    if (this.accumulatedDurationMs > CYCLE_DURATION_MS) {
+      this.toggleWipeMode()
+    }
+
     return footprintData
   }
 
   toggleWipeMode() {
-    this.tick = 0
+    this.accumulatedDurationMs = 0
     this.wipingInEllipse = !this.wipingInEllipse
   }
 }
