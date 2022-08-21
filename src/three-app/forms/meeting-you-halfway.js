3d4d79cc170e1d03699921afefda09c1de4c320c
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-08-21 08:41:52 +0100

    Update Meeting You Halfway to use delta time

--- src/three-app/forms/meeting-you-halfway.js
+++ src/three-app/forms/meeting-you-halfway.js
@@ -5,15 +5,16 @@ import { linearRamps } from "../ramps"
 import * as C from '../constants'
 import * as U from '../utils'
 
+const MAX_TICKS = 25000
+const CYCLE_DURATION_MS = C.TICK_DURATION_MS * MAX_TICKS
 const ELLIPSE_POINT_COUNT = 100
-const CYCLE_TICKS = 25000
 
 export class MeetingYouHalfwayForm {
 
   constructor(width, height) {
     this.width = width
     this.height = height
-    this.tick = 0
+    this.accumulatedDurationMs = 0
 
     const wipeOffsetMax = width * 0.4
     const wipeOffsetMin = -wipeOffsetMax
@@ -21,49 +22,52 @@ export class MeetingYouHalfwayForm {
     const ryMin = height / 4
 
     this.WIPE_OFFSET_BLOCKS = [
-      { span: CYCLE_TICKS * 0.25, from: 0, to: wipeOffsetMax },
-      { span: CYCLE_TICKS * 0.5, from: wipeOffsetMax, to: wipeOffsetMin },
-      { span: CYCLE_TICKS * 0.25, from: wipeOffsetMin, to: 0 }
+      { span: MAX_TICKS * 0.25, from: 0, to: wipeOffsetMax },
+      { span: MAX_TICKS * 0.5, from: wipeOffsetMax, to: wipeOffsetMin },
+      { span: MAX_TICKS * 0.25, from: wipeOffsetMin, to: 0 }
     ]
 
     this.WIPE_ROTATION_BLOCKS = [
-      { span: CYCLE_TICKS * 0.125, from: 0, to: -20 },
-      { span: CYCLE_TICKS * 0.125, from: -20, to: 0 },
-      { span: CYCLE_TICKS * 0.25, from: 0, to: 60 },
-      { span: CYCLE_TICKS * 0.25, from: 60, to: 0 },
-      { span: CYCLE_TICKS * 0.125, from: 0, to: -20 },
-      { span: CYCLE_TICKS * 0.125, from: -20, to: 0 }
+      { span: MAX_TICKS * 0.125, from: 0, to: -20 },
+      { span: MAX_TICKS * 0.125, from: -20, to: 0 },
+      { span: MAX_TICKS * 0.25, from: 0, to: 60 },
+      { span: MAX_TICKS * 0.25, from: 60, to: 0 },
+      { span: MAX_TICKS * 0.125, from: 0, to: -20 },
+      { span: MAX_TICKS * 0.125, from: -20, to: 0 }
     ]
 
     // B: slow expansion, fast contraction
     this.RY1_BLOCKS = [
-      { span: CYCLE_TICKS * 0.22, from: ryMin, to: ryMax },
-      { span: CYCLE_TICKS * 0.11, from: ryMax, to: ryMin },
-      { span: CYCLE_TICKS * 0.22, from: ryMin, to: ryMax },
-      { span: CYCLE_TICKS * 0.11, from: ryMax, to: ryMin },
-      { span: CYCLE_TICKS * 0.22, from: ryMin, to: ryMax },
-      { span: CYCLE_TICKS * 0.12, from: ryMax, to: ryMin }
+      { span: MAX_TICKS * 0.22, from: ryMin, to: ryMax },
+      { span: MAX_TICKS * 0.11, from: ryMax, to: ryMin },
+      { span: MAX_TICKS * 0.22, from: ryMin, to: ryMax },
+      { span: MAX_TICKS * 0.11, from: ryMax, to: ryMin },
+      { span: MAX_TICKS * 0.22, from: ryMin, to: ryMax },
+      { span: MAX_TICKS * 0.12, from: ryMax, to: ryMin }
     ]
 
     // A: slow contraction, fast expansion
     this.RY2_BLOCKS = [
-      { span: CYCLE_TICKS * 0.22, from: ryMax, to: ryMin },
-      { span: CYCLE_TICKS * 0.11, from: ryMin, to: ryMax },
-      { span: CYCLE_TICKS * 0.22, from: ryMax, to: ryMin },
-      { span: CYCLE_TICKS * 0.11, from: ryMin, to: ryMax },
-      { span: CYCLE_TICKS * 0.22, from: ryMax, to: ryMin },
-      { span: CYCLE_TICKS * 0.12, from: ryMin, to: ryMax }
+      { span: MAX_TICKS * 0.22, from: ryMax, to: ryMin },
+      { span: MAX_TICKS * 0.11, from: ryMin, to: ryMax },
+      { span: MAX_TICKS * 0.22, from: ryMax, to: ryMin },
+      { span: MAX_TICKS * 0.11, from: ryMin, to: ryMax },
+      { span: MAX_TICKS * 0.22, from: ryMax, to: ryMin },
+      { span: MAX_TICKS * 0.12, from: ryMin, to: ryMax }
     ]
   }
 
-  getFootprintData() {
-    const wipeOffset = linearRamps(this.WIPE_OFFSET_BLOCKS, this.tick)
-    const wipeRotationDegrees = linearRamps(this.WIPE_ROTATION_BLOCKS, this.tick)
+  getFootprintData(deltaMs) {
+    this.accumulatedDurationMs += deltaMs
+    const tick = this.accumulatedDurationMs / C.TICK_DURATION_MS
+
+    const wipeOffset = linearRamps(this.WIPE_OFFSET_BLOCKS, tick)
+    const wipeRotationDegrees = linearRamps(this.WIPE_ROTATION_BLOCKS, tick)
     const wipeRotationRadians = THREE.MathUtils.degToRad(wipeRotationDegrees)
 
     const rx = this.width / 2 - C.LINE_THICKNESS / 2
-    const ry1 = linearRamps(this.RY1_BLOCKS, this.tick)
-    const ry2 = linearRamps(this.RY2_BLOCKS, this.tick)
+    const ry1 = linearRamps(this.RY1_BLOCKS, tick)
+    const ry2 = linearRamps(this.RY2_BLOCKS, tick)
 
     const getEllipsePoints = ry => {
       const Δθ = C.TWO_PI / ELLIPSE_POINT_COUNT
@@ -83,13 +87,17 @@ export class MeetingYouHalfwayForm {
     const constant = wipeOffset
     const wipeClippingPlane1 = new THREE.Plane(normal, constant)
     const wipeClippingPlane2 = wipeClippingPlane1.clone().negate()
+
     const line1 = new Line(ellipse1Points, { clippingPlanes: [wipeClippingPlane1] })
     const line2 = new Line(ellipse2Points, { clippingPlanes: [wipeClippingPlane2] })
     const lines = [line1, line2]
 
-    this.tick++
-
     const footprintData = { lines }
+
+    if (this.accumulatedDurationMs > CYCLE_DURATION_MS) {
+      this.accumulatedDurationMs = 0
+    }
+
     return footprintData
   }
 }
