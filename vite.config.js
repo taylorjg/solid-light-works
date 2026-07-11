45b85a09d526573595ede71ea7f4c35f9623736e
Jonathan Taylor <jonathan.taylor@disney.com>
2026-07-11 10:02:04 +0100

    Remove redundant bit of vite config

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,6 @@ export default defineConfig({
   },
   plugins: [react()],
   base: '/solid-light-works/',
-  build: {
-    outDir: 'dist',
-  },
   test: {
     globals: true,
     environment: 'jsdom',
